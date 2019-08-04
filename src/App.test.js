a01ad5ee089bc16791053798a0965dede4bb30ed
Semmiverian <semmivp1@gmail.com>
2019-08-04 09:00:26 +0700

    commit with testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import {render} from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+  ReactDOM.unmountComponentAtNode(div)
+})
+
+it('renders with react testing library', () => {
+  const {getByText} = render(<App />)
+
+  expect(getByText('Learn React')).toBeInTheDocument()
+})
