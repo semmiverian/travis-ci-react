d81ce6ad58c61ea9c80eb36170385e06b840ebde
Semmiverian <semmivp1@gmail.com>
2019-08-04 09:10:56 +0700

    bahasa indonesia lebih bagus

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
+          Bahasa React
         </a>
       </header>
     </div>
